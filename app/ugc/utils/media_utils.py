40d91bcc5984eb62c9592db1dbb050042f6b6561
dmtryii <dmitriy04tr@gmail.com>
2023-12-16 22:13:11 +0200

    removing unnecessary functionality

--- app/ugc/utils/media_utils.py
+++ app/ugc/utils/media_utils.py
@@ -1,8 +1,6 @@
 import asyncio
-import os
 
 from django.conf import settings
-from moviepy.video.io.VideoFileClip import VideoFileClip
 from pytube import YouTube
 
 
@@ -13,28 +11,6 @@ async def download_video(url: str) -> str | None:
         output_path = settings.MEDIA_FILES
         file_path = await asyncio.to_thread(stream.download, output_path=output_path)
         return file_path
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
-
-
-async def convert_video_to_audio(url: str) -> str | None:
-    try:
-        video_path = await download_video(url)
-        if video_path is None:
-            return None
-
-        audio_path = os.path.splitext(os.path.basename(video_path))[0] + '.mp3'
-
-        video_clip = VideoFileClip(video_path)
-        audio_clip = video_clip.audio
-        audio_clip.write_audiofile(audio_path, codec='mp3')
-
-        video_clip.close()
-        audio_clip.close()
-        os.remove(video_path)
-
-        return audio_path
     except Exception as e:
         print(f"An error occurred: {e}")
         return None

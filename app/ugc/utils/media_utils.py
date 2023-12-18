7dbf1d75d16abf7671c46a6fb3f6ec05503518cf
dmtryii <dmitriy04tr@gmail.com>
2023-12-18 17:10:01 +0200

    added audio only download

--- app/ugc/utils/media_utils.py
+++ app/ugc/utils/media_utils.py
@@ -18,6 +18,18 @@ async def download_video(url: str) -> str | None:
         return None
 
 
+async def download_audio(url: str) -> str | None:
+    try:
+        video = YouTube(url)
+        stream = video.streams.filter(only_audio=True).first()
+        output_path = settings.MEDIA_FILES
+        file_path = await asyncio.to_thread(stream.download, output_path=output_path)
+        return file_path
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
 async def get_media_info_cart(media: Media) -> str:
     duration = divmod(media.duration, 60)
     return (f'{media.title}\n\n' +

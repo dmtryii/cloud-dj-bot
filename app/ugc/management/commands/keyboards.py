30fe7aed97073a0b0cb3aa364cb95334a70f4c21
dmtryii <dmitriy04tr@gmail.com>
2023-12-20 10:38:25 +0200

    Added favorite button

--- app/ugc/management/commands/keyboards.py
+++ app/ugc/management/commands/keyboards.py
@@ -1,13 +1,29 @@
 from enum import Enum
 
 from aiogram.filters.callback_data import CallbackData
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="History")
+        ],
+[
+            KeyboardButton(text="Favorite")
+        ],
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True,
+    input_field_placeholder='Choose an action from the menu'
+)
 
 
 class Action(str, Enum):
     VIDEO_DOWNLOAD = 'video'
     AUDIO_DOWNLOAD = 'audio'
+    MEDIA_TO_FAVORITES = 'favorite'
 
 
 class Navigation(str, Enum):
@@ -23,6 +39,12 @@ class SelectDownloadType(CallbackData, prefix="dow"):
 class Pagination(CallbackData, prefix="pag"):
     navigation: Navigation
     page: int
+    types: str
+
+
+class Favorite(CallbackData, prefix="fav"):
+    action: Action
+    media_id: int
 
 
 def select_download_type(media_id: int) -> InlineKeyboardMarkup:
@@ -38,13 +60,20 @@ def select_download_type(media_id: int) -> InlineKeyboardMarkup:
     return builder.as_markup()
 
 
-def pagination(media_id: int, page: int = 0) -> InlineKeyboardMarkup:
+def media_pagination(media_id: int, types: str, page: int = 0, total_pages: int = 0) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.row(
         InlineKeyboardButton(text='<',
-                             callback_data=Pagination(navigation=Navigation.PREV_STEP, page=page - 1).pack()),
+                             callback_data=Pagination(navigation=Navigation.PREV_STEP,
+                                                      types=types,
+                                                      page=page - 1).pack()),
+        InlineKeyboardButton(text=f'{page + 1}/{total_pages}',
+                             callback_data=f'{page + 1}/{total_pages}'),
+
         InlineKeyboardButton(text='>',
-                             callback_data=Pagination(navigation=Navigation.NEXT_STEP, page=page + 1).pack())
+                             callback_data=Pagination(navigation=Navigation.NEXT_STEP,
+                                                      types=types,
+                                                      page=page + 1).pack())
     )
     builder.row(
         InlineKeyboardButton(text='Video',
@@ -53,5 +82,10 @@ def pagination(media_id: int, page: int = 0) -> InlineKeyboardMarkup:
         InlineKeyboardButton(text='Audio',
                              callback_data=SelectDownloadType(action=Action.AUDIO_DOWNLOAD,
                                                               media_id=media_id).pack())
+    )
+    builder.row(
+        InlineKeyboardButton(text='Favorite',
+                             callback_data=Favorite(action=Action.MEDIA_TO_FAVORITES,
+                                                    media_id=media_id).pack())
     )
     return builder.as_markup()

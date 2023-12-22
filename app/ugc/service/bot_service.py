ad4e26e5ad48b15b450518f0827fff51bc8e2c17
dmtryii <dmitriy04tr@gmail.com>
2023-12-22 15:09:57 +0200

    refactoring

--- app/ugc/service/bot_service.py
+++ app/ugc/service/bot_service.py
@@ -15,34 +15,45 @@ async def send_media(chat_id: int, media: Media, send_media_func, send_warning_f
     file_id = getattr(media, file_id_attr, None)
 
     if file_id:
-        await send_media_func(chat_id=chat_id,
-                              caption=caption,
-                              **{file_id_attr.split('_')[1]: file_id})
+        await send_existing_media(chat_id, send_media_func, file_id, caption)
     else:
-        profile = await get_profile_by_external_id(external_id=chat_id)
+        await process_new_media(chat_id, media, send_media_func, send_warning_func, file_id_attr, caption, warning)
 
-        if not await can_download_media(profile):
-            await send_warning_func(chat_id, warning)
-            return
 
-        await add_download_media(profile, media)
+async def send_existing_media(chat_id, send_media_func, file_id, caption):
+    try:
+        await send_media_func(chat_id=chat_id, caption=caption, **{file_id.split('_')[1]: file_id})
+    except Exception as e:
+        print(f"Error sending existing media: {e}")
 
-        path = None
-        try:
-            download_func = download_video if send_media_func.__name__ == 'send_video' else download_audio
 
-            path = await download_func(media.url)
-            msg = await send_media_func(chat_id=chat_id,
-                                        caption=caption,
-                                        **{file_id_attr.split('_')[1]: FSInputFile(path)})
-            setattr(media, file_id_attr, getattr(msg, file_id_attr.split('_')[1]).file_id)
-            await media.asave()
-        except Exception as e:
-            print(f"Error sending media: {e}")
-        finally:
-            await asyncio.sleep(1)
-            try:
-                if path:
-                    os.remove(path)
-            except Exception as e:
-                print(f"Error removing file: {e}")
+async def process_new_media(chat_id, media, send_media_func, send_warning_func, file_id_attr, caption, warning):
+    profile = await get_profile_by_external_id(external_id=chat_id)
+
+    if not await can_download_media(profile):
+        await send_warning_func(chat_id, warning)
+        return
+
+    await add_download_media(profile, media)
+
+    path = None
+    try:
+        download_func = download_video if send_media_func.__name__ == 'send_video' else download_audio
+        path = await download_func(media.url)
+
+        msg = await send_media_func(chat_id=chat_id, caption=caption, **{file_id_attr.split('_')[1]: FSInputFile(path)})
+        setattr(media, file_id_attr, getattr(msg, file_id_attr.split('_')[1]).file_id)
+        await media.asave()
+    except Exception as e:
+        print(f"Error sending new media: {e}")
+    finally:
+        await cleanup_file(path)
+
+
+async def cleanup_file(path):
+    await asyncio.sleep(1)
+    try:
+        if path:
+            os.remove(path)
+    except Exception as e:
+        print(f"Error removing file: {e}")
